d83fe261158a7ac18b7f754cd43c172e5bd40446
Inessa Vasilevskaya <inavasilevskaya@gmail.com>
2022-01-28 13:04:55 +0000

    Add error_handler
    Shamelessly borrow one from python-telegram-bot examples.
    The handler will send whatever error appears as a telegram
    message to the developer.

--- a2exams_bot.py
+++ a2exams_bot.py
@@ -11,6 +11,7 @@ import check_a2_slots
 
 UPDATE_INTERVAL = 20
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+DEVELOPER_CHAT_ID = os.getenv('DEVELOPER_CHAT_ID')
 
 old_data = check_a2_slots.get_schools_from_file()
 REDIS = redis.from_url(os.getenv('REDIS_URL', 'redis://redis:6379'))
@@ -83,6 +84,34 @@ def inform_about_change(context: CallbackContext) -> None:
     old_data = new_data
 
 
+# NOTE(ivasilev) Shamelessly borrowed from
+# https://github.com/python-telegram-bot/python-telegram-bot/blob/master/examples/errorhandlerbot.py
+def error_handler(update: object, context: CallbackContext) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = ''.join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f'An exception was raised while handling an update\n'
+        f'<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}'
+        '</pre>\n\n'
+        f'<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n'
+        f'<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n'
+        f'<pre>{html.escape(tb_string)}</pre>'
+    )
+
+    # Finally, send the message
+    context.bot.send_message(chat_id=DEVELOPER_CHAT_ID, text=message, parse_mode=ParseMode.HTML)
+
+
 def run():
     updater = Updater(TOKEN)
     updater.dispatcher.add_handler(CommandHandler('check', check))
@@ -90,6 +119,7 @@ def run():
     updater.dispatcher.add_handler(CommandHandler('track', track))
     updater.dispatcher.add_handler(CommandHandler('notrack', notrack))
     updater.dispatcher.add_handler(CommandHandler('mystatus', mystatus))
+    updater.dispatcher.add_error_handler(error_handler)
     updater.job_queue.run_repeating(inform_about_change, interval=UPDATE_INTERVAL, first=0)
     updater.start_polling()
     updater.idle()
